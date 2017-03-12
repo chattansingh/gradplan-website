04cf639c299111310aca0c22e3d04d0af6b9549c
evance <ericr.vance@gmail.com>
2017-03-12 10:56:15 -0700

    Roadmap HTML is displaying the Test data correctly

--- plan/views.py
+++ plan/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from testdata import road_map, test_list
+from testdata import road_map
 
 def grad_road_map(request):
 
 
-    context = {'road_map': road_map, 'test_list': test_list}
+    context = {'road_map': road_map}
     template = 'plan/graduation_roadmap.html'
     return render(request, template, context)
