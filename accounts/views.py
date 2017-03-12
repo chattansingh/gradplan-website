e194bf89c9fdc0b342b6a6f0491763b847dfff5b
evance <ericr.vance@gmail.com>
2017-03-12 10:35:38 -0700

    Suggest Major Choices getting form data

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render
-from forms import InterestsForm
+from forms import InterestsForm, SuggestMajor
 from models import Profile
 
 
@@ -32,15 +32,27 @@ def update_profile(request):
 
 
 def suggest_major(request):
+    current_user = Profile.objects.get(user=request.user)
+
     if request.method == 'POST':
-        form = InterestsForm(request.POST, instance=request.user)
+
+        form = SuggestMajor(request.POST, instance=current_user)
 
         if form.is_valid():
+            result = 'valid save '
+            #this needs to change to point to where we want to display their grad plan
             template = 'accounts/suggest_major.html'
             form.save()
+            #this is a list of the choices
+            #choices are 1, 2, 3, 4 1 = Science, 2 = Math..and so on.
+            choices = form.cleaned_data['subject_interests']
+            result = result + choices[0]
         else:
-            template = 'accounts/suggest_major.html'
+
+            template = 'accounts/save_error.html'
     else:
+        result = ' not post'
         template = 'accounts/suggest_major.html'
-        form = InterestsForm(instance=request.user)
-    return render(request, template, {'form': form,})
+        choices = []
+        form = SuggestMajor(instance=current_user)
+    return render(request, template, {'form': form, 'result': result, 'interests' : choices})
