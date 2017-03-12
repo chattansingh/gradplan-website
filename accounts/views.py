55747150e60034918c344e629791f3551ea138f0
evance <ericr.vance@gmail.com>
2017-03-12 10:01:42 -0700

    I think views and models are working now

--- accounts/views.py
+++ accounts/views.py
@@ -5,54 +5,30 @@ from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render
 from forms import InterestsForm
+from models import Profile
 
 
 # Create your views here.
 @login_required
 # @transaction.atomic
 def update_profile(request):
+    current_user = Profile.objects.get(user=request.user)
     if request.method == 'POST':
-        form = InterestsForm(request.POST, instance=request.user)
+        form = InterestsForm(request.POST, instance=current_user)
 
         if form.is_valid():
+            result = 'valid save'
             template = 'accounts/profile_form.html'
             form.save()
         else:
+            result = 'not valid form'
             template = 'accounts/save_error.html'
     else:
+        result ='not post'
         template = 'accounts/profile_form.html'
-        form = InterestsForm(instance=request.user)
-    return render(request, template, {'form': form,})
-
-
-def suggest_major(request):
-    if request.method == 'POST':
-        form = InterestsForm(request.POST, instance=request.user)
-
-        if form.is_valid():
-            template = 'accounts/suggest_major.html'
-            form.save()
-        else:
-            template = 'accounts/suggest_major.html'
-    else:
-        template = 'accounts/suggest_major.html'
-        form = InterestsForm(instance=request.user)
-    return render(request, template, {'form': form,})
-@login_required
-# @transaction.atomic
-def update_profile(request):
-    if request.method == 'POST':
-        form = InterestsForm(request.POST, instance=request.user)
-
-        if form.is_valid():
-            template = 'accounts/profile_form.html'
-            form.save()
-        else:
-            template = 'accounts/save_error.html'
-    else:
-        template = 'accounts/profile_form.html'
-        form = InterestsForm(instance=request.user)
-    return render(request, template, {'form': form,})
+        # current_user = Profile.objects.get(request.user)
+        form = InterestsForm(instance=current_user)
+    return render(request, template, {'form': form, 'result': result})
 
 
 def suggest_major(request):
