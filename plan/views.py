9983634ca30b768ce0da57368b308a4ed1273f4f
evance <ericr.vance@gmail.com>
2017-03-12 11:23:14 -0700

    Roadmap displaying dynamic scraping of data

--- plan/views.py
+++ plan/views.py
@@ -6,7 +6,9 @@ from gradplan import getroadmap
 
 def grad_road_map(request):
 
-    road_map = getroadmap('http://catalog.csun.edu/academics/comp/programs/bs-computer-science/', {})
+    #This needs to have the dynamic url that is passed based off the users gradplan
+    url = 'http://catalog.csun.edu/academics/comp/programs/bs-computer-science/'
+    road_map = getroadmap( url, {})
     context = {'road_map': road_map}
     template = 'plan/graduation_roadmap.html'
     return render(request, template, context)
