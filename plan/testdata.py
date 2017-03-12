4ee8da318f1f2963c7649b7e5a3b923b7eed10bb
evance <ericr.vance@gmail.com>
2017-03-12 10:40:17 -0700

    Roadmap html and test data

--- plan/testdata.py
+++ plan/testdata.py
@@ -1,24 +1,9 @@
-road_map = { 'year1' :
-                 {'semester1' :
-                      {'classes' : ['Comp 110', 'Math150A', 'GRE Basic Skills', 'Title5 Requirement']},
-                  'semester2' :
-                      {'classes' : ['Comp 122', 'Comp 182', 'Math 150B', 'Phil 230']}
-                  }
-             }
-
-test_list =[
-    {'semester': 1,
-     'classes': ['Comp 110',
-                 'Math150A',
-                 'GRE Basic Skills',
-                 'Title5 Requirement'
-                 ]
-     },
-    {'semester': 2,
-     'classes' : ['Comp 122',
-                  'Comp 182',
-                  'Math 150B',
-                  'Phil 230'
-                  ]
+road_map = [
+    {'classes': [
+         {'name': 'Comp 110', 'start_time': ['15:00', '19:00'], 'end_time': ['18:00', '20:00'], 'course_id': ['12345', '56789'], 'location': ['JD1145', 'JD4353']},
+         {'name': 'Math150A', 'start_time': ['15:00', '19:00'], 'end_time': ['18:00', '20:00'], 'course_id': ['12345', '56789'], 'location': ['JD1145', 'JD4353']},
+         {'name': 'GRE Basic Skills', 'start_time': ['07:00', '04:00'], 'end_time': ['23:00', '20:00'], 'course_id': ['12345', '56789'], 'location': ['JD1145', 'JD4353']},
+         {'name': 'Title5 Requirement', 'start_time': ['09:00', '10:00'], 'end_time': ['20:00', '20:00'], 'course_id': ['12345', '56789'], 'location': ['JD1145', 'JD4353']},
+     ]
      },
 ]
