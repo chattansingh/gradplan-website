8dcc52c8812842079a2f1fd6b13914260cef5aa3
evance <ericr.vance@gmail.com>
2017-03-13 00:00:57 -0700

    Manipulated the data being sent back to break into semester so that I can fit into jesus html Added all the links to the new HTML they all point to a specific url

--- plan/views.py
+++ plan/views.py
@@ -12,7 +12,28 @@ def grad_road_map(request):
     #This needs to have the dynamic url that is passed based off the users gradplan
     # url = 'http://catalog.csun.edu/academics/comp/programs/bs-computer-science/'
     road_map = getroadmap( url, {})
-    context = {'road_map': road_map}
+
+    #need to split up the roadmap
+    counter = 1
+    year1 =[]
+    year2 =[]
+    year3 =[]
+    year4 =[]
+
+    for semester in road_map:
+        if counter == 1 or counter ==2:
+            year1.append(semester)
+        elif counter == 3 or counter == 4:
+            year2.append(semester)
+        elif counter == 5 or counter == 6:
+            year3.append(semester)
+        elif counter == 7 or counter == 8:
+            year4.append(semester)
+        counter = counter + 1
+
+
+
+    context = {'road_map': road_map, 'year1':year1, 'year2':year2,'year3':year3, 'year4':year4}
     # template = 'plan/graduation_roadmap.html'
     template = 'plan/Plans.html'
     return render(request, template, context)
