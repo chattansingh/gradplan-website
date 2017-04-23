7491e681497e846d8799534b30c793638d2daa5e
Hunt4Bugs <nanokid92@gmail.com>
2017-04-23 21:44:39 +0000

    added filtering of class times

--- plan/gradtest.py
+++ plan/gradtest.py
@@ -2,7 +2,8 @@ from gradplan import *
 
 plans = getbaseplans()
 print len(plans)
-changeplan(plans[0])
+e = { 'days': ['Tu','Th'], 'times':[['09:00 AM'], ['09:00 AM']], 'taken':['MATH 150A']}
+changeplan(plans[0], [], e)
 #uncomment lines below to see example output for CS
 #e = { 'days': ['Tu','Th'], 'times':[['09:00 AM'], ['09:00 AM']], 'taken':['MATH 150A']}
 # e = {'days':[], 'times':[], 'taken': []}
