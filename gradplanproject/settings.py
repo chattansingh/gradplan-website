5a077fae4d185151b4a46fa81012aff1f774e81c
evance <ericr.vance@gmail.com>
2017-05-10 09:45:34 -0700

    trying to fix css issues on main server

--- gradplanproject/settings.py
+++ gradplanproject/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #Logged in user redriect
 LOGIN_REDIRECT_URL = '/'
